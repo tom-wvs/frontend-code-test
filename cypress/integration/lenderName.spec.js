fbf68ee8cef851c8e4db55680e54ef09ea18f102
Joan Miquel <joan.miquel@ip-192-168-1-80.eu-west-1.compute.internal>
2020-07-31 18:26:51 +0200

    feat: add cypress

---
+++ cypress/integration/lenderName.spec.js
@@ -0,0 +1,43 @@
+import getLendersRecipe from '../fixtures/lenders';
+
+context('Home Page', () => {
+  let lenders = null;
+  before(() => {
+    lenders = getLendersRecipe.default();
+  });
+
+  beforeEach(() => {
+    const url = Cypress.env('host');
+    cy.visit(url);
+  });
+
+  it('will check if all important elements are in place:', () => {
+    // Names and links for each banck should be visible for the user:
+    lenders.forEach((lender) => {
+      cy.get(`[data-cy="${lender.slug}"]`)
+        .should('be.visible')
+        .should('contain', lender.name);
+    });
+  });
+
+  it('will check if all important links are in place and take the user to correct URL', () => {
+    // Names and links for each banck should be visible for the user:
+    lenders.forEach((lender) => {
+      cy.get(`[data-cy="${lender.slug}"]`)
+        .should('be.visible')
+        .should('contain', lender.name);
+    });
+
+    cy.get(`[data-cy="${lenders[0].slug}"]`).click();
+    cy.location('pathname').should('be', `/${lenders[0].slug}`);
+    cy.go('back');
+
+    cy.get(`[data-cy="${lenders[1].slug}"]`).click();
+    cy.location('pathname').should('be', `/${lenders[1].slug}`);
+    cy.go('back');
+
+    cy.get(`[data-cy="${lenders[2].slug}"]`).click();
+    cy.location('pathname').should('be', `/${lenders[2].slug}`);
+    cy.go('back');
+  });
+});
