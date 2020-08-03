10db6f6dc8c593e923a6b6f2f0b229494e3ad62d
Joan Miquel <joan.miquel@Joans-MBP.home>
2020-08-03 12:50:14 +0200

    feat: add cypress and tests

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,9 +1,9 @@
-import getLendersRecipe from '../fixtures/lenders';
+import getLenders from '../fixtures/lenders';
 
 context('Home Page', () => {
   let lenders = null;
   before(() => {
-    lenders = getLendersRecipe.default();
+    lenders = getLenders();
   });
 
   beforeEach(() => {
@@ -12,31 +12,31 @@ context('Home Page', () => {
   });
 
   it('will check if all important elements are in place:', () => {
-    // Names and links for each banck should be visible for the user:
+    // Names and links for each bank should be visible for the user:
     lenders.forEach((lender) => {
-      cy.get(`[data-cy="${lender.slug}"]`)
+      cy.get(`[data-testid="${lender.slug}"]`)
         .should('be.visible')
         .should('contain', lender.name);
     });
   });
 
   it('will check if all important links are in place and take the user to correct URL', () => {
-    // Names and links for each banck should be visible for the user:
+    // Names and links for each bank should be visible for the user:
     lenders.forEach((lender) => {
-      cy.get(`[data-cy="${lender.slug}"]`)
+      cy.get(`[data-testid="${lender.slug}"]`)
         .should('be.visible')
         .should('contain', lender.name);
     });
 
-    cy.get(`[data-cy="${lenders[0].slug}"]`).click();
+    cy.get(`[data-testid="${lenders[0].slug}"]`).click();
     cy.location('pathname').should('be', `/${lenders[0].slug}`);
     cy.go('back');
 
-    cy.get(`[data-cy="${lenders[1].slug}"]`).click();
+    cy.get(`[data-testid="${lenders[1].slug}"]`).click();
     cy.location('pathname').should('be', `/${lenders[1].slug}`);
     cy.go('back');
 
-    cy.get(`[data-cy="${lenders[2].slug}"]`).click();
+    cy.get(`[data-testid="${lenders[2].slug}"]`).click();
     cy.location('pathname').should('be', `/${lenders[2].slug}`);
     cy.go('back');
   });
